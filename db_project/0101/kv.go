9d885356b9943a1271c840a96b0b3b114c802f61
Lukas Aufmesser <299863947+ZakeCode@users.noreply.github.com>
2026-07-05 22:52:27 +0200

    feat: 0101 test success after checking test for error handling... -.-

--- db_project/0101/kv.go
+++ db_project/0101/kv.go
@@ -1,5 +1,9 @@
 package db0101
 
+import (
+	"fmt"
+)
+
 type KV struct {
 	mem map[string][]byte
 }
@@ -11,10 +15,39 @@ func (kv *KV) Open() error {
 
 func (kv *KV) Close() error { return nil }
 
-func (kv *KV) Get(key []byte) (val []byte, ok bool, err error)
+func (kv *KV) Get(key []byte) (val []byte, ok bool, err error) {
+	if val = kv.mem[string(key)]; val != nil {
+		ok = true
+	} else {
+		ok = false
+		// err = errors.New("DB: Could not find key " + string(key) + " in memory")
+	}
+	fmt.Println(string(key), val, ok, err)
+	return val, ok, err
+}
 
-func (kv *KV) Set(key []byte, val []byte) (updated bool, err error)
+func (kv *KV) Set(key []byte, val []byte) (updated bool, err error) {
+	if kv.mem[string(key)] != nil {
+		updated = false
+		// err = errors.New("DB: Key " + string(key) + " already exists!")
+	} else {
+		kv.mem[string(key)] = val
+		updated = true
+	}
+	fmt.Println(string(key), updated, err)
+	return updated, err
+}
 
-func (kv *KV) Del(key []byte) (deleted bool, err error)
+func (kv *KV) Del(key []byte) (deleted bool, err error) {
+	if kv.mem[string(key)] != nil {
+		kv.mem[string(key)] = nil
+		deleted = true
+	} else {
+		deleted = false
+		// err = errors.New("DB: Key " + string(key) + " does not exist!")
+	}
+	fmt.Println(string(key), deleted, err)
+	return deleted, err
+}
 
 // QzBQWVJJOUhU https://trialofcode.org/
